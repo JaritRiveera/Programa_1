0a0125a71250b18d1c2b768bfccb386b1fb30749
JaritRiveera <jarit.es_0407@hotmail.com>
2017-03-01 17:08:33 -0600

    Update Programa1.1.py
    Segunda corrección.

--- Programa1.1.py
+++ Programa1.1.py
@@ -1,10 +1,10 @@
 
-cadena=('las zanahorias y las arroz son buenos alimentos')
+cadena=('las zanahorias y el arroz son buenos alimentos')
 contar=0
 for i in cadena:
     if(i=='z'):
         contar=contar+1
         print(contar)
-cadena1=('las zanahorias y las zandias son buenos alimentos')
+cadena1=('las zanahorias y el arroz son buenos alimentos')
 for i in cadena1:
     print(i)
